7df4cd9ae16904da59550b8278a81e9856f9cb96
Kamil <kamilm2003@gmail.com>
2024-07-07 12:04:41 +0200

    basic nav fade in animation

--- components/navigation/Hamburger.tsx
+++ components/navigation/Hamburger.tsx
@@ -1,27 +1,34 @@
 import React, { useState } from "react";
 
-export default function Hamburger({ isClicked }: { isClicked: boolean }) {
+export default function Hamburger({
+  isClicked,
+  clickHandler,
+}: {
+  isClicked: boolean;
+  clickHandler: ([any]: any) => any;
+}) {
   return (
     <button
+      onClick={clickHandler}
       className={`flex flex-col justify-center items-center gap-1 w-14 aspect-square rounded-full transition-colors ${
-        isClicked ? "bg-black" : "bg-black"
+        !isClicked ? "bg-black" : "bg-black"
       }`}
     >
       <span
         className={`block h-1 w-8 rounded-full bg-white transform ${
-          isClicked
+          !isClicked
             ? "animate-[open-menu-upper-line-forward_.25s_ease-in-out_forwards]"
             : "animate-[open-menu-upper-line-backward_.25s_ease-in-out]"
         }`}
       ></span>
       <span
         className={`block h-1 w-8 rounded-full bg-white transition-opacity ${
-          isClicked ? "opacity-0" : "opacity-100"
+          !isClicked ? "opacity-0" : "opacity-100"
         }`}
       ></span>
       <span
         className={`block h-1 w-8 rounded-full bg-white duration-[175ms] ${
-          isClicked
+          !isClicked
             ? "animate-[open-menu-lower-line-forward_.25s_ease-in-out_forwards]"
             : "animate-[open-menu-lower-line-backward_.25s_ease-in-out]"
         }`}
--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -1,19 +1,55 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Hamburger from "@/components/navigation/Hamburger";
 import Link from "next/link";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 export default function Navigation() {
   const [navIsOpen, setNavIsOpen] = useState(true);
+
+  const container = useRef(null);
+  const tl = useRef<any>(null);
+
+  useGSAP(
+    () => {
+      // Initial states
+      gsap.set(container.current, { autoAlpha: 0 });
+
+      tl.current = gsap.timeline({ paused: true });
+      // Toggling menu animation
+      tl.current.to(container.current, { autoAlpha: 1 });
+    },
+    { scope: container }
+  );
+
+  useEffect(() => {
+    if (!tl.current.isActive())
+      if (!navIsOpen) {
+        // Opening nav
+        tl.current.play();
+      } else {
+        tl.current.reverse();
+      }
+  }, [navIsOpen]);
 
   return (
     <nav className="fixed z-[999] top-0 left-0 w-full flex justify-end items-center">
       {/* <div>PL</div>
       <div>Light</div> */}
 
-      <Hamburger isClicked={navIsOpen} />
+      <Hamburger
+        clickHandler={() => {
+          if (tl.current.isActive()) return;
+          setNavIsOpen((prev) => !prev);
+        }}
+        isClicked={navIsOpen}
+      />
 
-      <div className="flex justify-start items-center pl-44 bg-black text-white absolute -z-[1] w-full h-screen left-0 top-0">
+      <div
+        ref={container}
+        className="flex justify-start items-center pl-44 bg-black text-white absolute -z-[1] w-full h-screen left-0 top-0"
+      >
         <ul className="flex flex-col gap-7 text-6xl ">
           <li>
             <Link className="nav-link" href="/">
